ce9b127392a8ca1f7bad40fd0b674d88b933fadc
Steve Rector <steverector27@gmail.com>
2024-02-15 10:43:54 -0800

    add src folder structure

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 import express from "express";
 
+// express setup
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
